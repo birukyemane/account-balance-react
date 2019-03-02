f6526ea1cae9fb6841a5b75e1a32597e063fda48
birukyemane <bir_yem@yahoo.com>
2019-03-02 19:48:04 +0200

    minor fix

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-class Transaction extends React.Component  {
+class Transaction extends React.Component  { 
 
     constructor(props) {
         super(props);
