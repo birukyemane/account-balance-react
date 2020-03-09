c24e2982b92f713e9694bfa95b8b4d1273604777
birukyemane <bir_yem@yahoo.com>
2020-03-09 22:08:47 +0200

    -  add transactions compnent

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,4 +1,4 @@
-// lists transactions. its the column of either incomes or expenses 
+// lists transactions. its the column of either incomes or expenses   
 import React from 'react';
 import Transaction from './Transaction'
 
